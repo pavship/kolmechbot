5808de729af81c074f18de2fd68f87b4cafe148f
PS <pavship@yandex.ru>
2019-06-14 00:13:03 +0300

    bot config

--- bot.js
+++ bot.js
@@ -4,7 +4,10 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const bot = new TelegramBot(
   process.env.TELEGRAM_KOLMECHBOT_TOKEN,
-  {polling: true}
+  {webHook: { 
+    port: process.env.PORT || 8443,
+    host : process.env.HOST || '0.0.0.0'
+  }}
 )
 
 console.log('bot > ', bot)
